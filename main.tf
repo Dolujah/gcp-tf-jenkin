13f84f3797e039915a6eb92c336d55ed74401798
Ruth Uj <119963110+Dolujah@users.noreply.github.com>
2024-08-15 14:39:31 -0500

    Update main.tf
    Changed the google storage bucket name

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-resource "google_storage_bucket" "my-bucket" {
-  name                     = "tt-githubdemo-bucket-001"
+resource "google_storage_bucket" "my-bucketq1" {
+  name                     = "tt-githubdemo-bucket-0011"
   project                  = "tt-dev-001"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
 #
-resource "google_storage_bucket" "my-bucket2" {
-  name                     = "tt-githubdemo-bucket-002"
+resource "google_storage_bucket" "my-bucket2q1" {
+  name                     = "tt-githubdemo-bucket-0022"
   project                  = "tt-dev-001"
   location                 = "US"
   force_destroy            = true
